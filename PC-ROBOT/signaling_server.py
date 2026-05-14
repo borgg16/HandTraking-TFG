a82d4141cc866cb278ec6998d2bcb892cbbd477d
borgg16 <borjaaa2001@gmail.com>
2026-05-14 20:25:16 +0200

    Cambio de IP simplemente en el codigo, pero todo correcto ya

--- PC-ROBOT/signaling_server.py
+++ PC-ROBOT/signaling_server.py
@@ -14,7 +14,7 @@ log = logging.getLogger(__name__)
 #---------------------------------------
 #CONFIGURACION
 #---------------------------------------
-HOST = "0.0.0.0"
+HOST = "192.168.1.124"
 #Ponerlo todo a 0.0.0.0 significa que acepta conexiones desde cualquier IP de la red
 #Si solo queremos aceptar  conexiones locales(mismo PC), usamos "127.0.0.1"
 
